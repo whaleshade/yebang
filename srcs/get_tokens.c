525e66bbdee69c7a9481ceb0605ec4e96c314e20
whaleshade <whaleshade@gmail.com>
2022-08-17 19:52:34 +0900

    parenthesis token

--- srcs/get_tokens.c
+++ srcs/get_tokens.c
@@ -6,7 +6,7 @@
 /*   By: jibang <jibang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 13:39:55 by jibang            #+#    #+#             */
-/*   Updated: 2022/08/17 19:16:58 by jibang           ###   ########.fr       */
+/*   Updated: 2022/08/17 19:52:13 by jibang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,38 @@ void	make_s_quote_token(char *line, int *i, t_list **token_list)
 	lstadd_token_node(ft_substr(line, *i - len - 1, len + 1), token_list);
 }
 
+void	make_parenthesis_token(char *line, int *i, t_list **token_list)
+{
+	int		len;
+	int		lcnt;
+	int		rcnt;
+
+	lcnt = 1;
+	rcnt = 0;
+	(*i)++;
+	len = 0;
+	while (line[*i] && line[*i] != ')')
+	{
+		len++;
+		(*i)++;
+		if (line[*i] == '(')
+			lcnt++;
+	}
+	if (line[*i] == ')')
+	{
+		rcnt++;
+		while (line[*i] && (lcnt != rcnt))
+		{
+			len++;
+			(*i)++;
+			if (line[*i] == ')')
+				rcnt++;
+		}
+		len++;
+		(*i)++;
+	}
+	lstadd_token_node(ft_substr(line, *i - len - 1, len + 1), token_list);
+}
 
 void	make_tokens_list(const char *str, t_list **token_list)
 {
@@ -81,13 +113,12 @@ void	make_tokens_list(const char *str, t_list **token_list)
 		{
 			/* quotation case */
 			if (line[i] == '\"')
-			{
 				make_d_quote_token(line, &i, token_list);
-			}
 			if (line[i] == '\'')
-			{
 				make_s_quote_token(line, &i, token_list);
-			}
+			/* parenthesis case */
+			if (line[i] == '(')
+				make_parenthesis_token(line, &i, token_list);
 			/* | case */
 			if (line[i] == '|' && line[i] != '|')
 				lstadd_token_node(ft_substr(line, i, 1), token_list);
@@ -124,7 +155,6 @@ void	make_tokens_list(const char *str, t_list **token_list)
 			lstadd_token_node(ft_substr(line, i, 1), token_list);
 			i++;
 		}
-
 	}
 }
 
