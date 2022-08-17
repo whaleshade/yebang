bd63024338d8073e1454af1f642a009909cae314
whaleshade <whaleshade@gmail.com>
2022-08-17 17:09:34 +0900

    make alpha token

--- srcs/get_tokens.c
+++ srcs/get_tokens.c
@@ -6,7 +6,7 @@
 /*   By: jibang <jibang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 13:39:55 by jibang            #+#    #+#             */
-/*   Updated: 2022/08/16 21:49:24 by jibang           ###   ########.fr       */
+/*   Updated: 2022/08/17 17:09:09 by jibang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,31 @@
 
 void	lstadd_token_node(char *token, t_list **token_list);
 
+void	make_alpha_token(char *line, int *i, t_list **token_list)
+{
+	int		len;
+
+	len = 0;
+	while (line[*i] && ft_isalpha(line[*i]))
+	{
+		len++;
+		(*i)++;
+	}
+	lstadd_token_node(ft_substr(line, *i - len, len), token_list);
+}
+
+
 void	make_tokens_list(const char *str, t_list **token_list)
 {
 	char	*line;
 	int		i;
-	int		len;
+	// int		len;
 
 	line = ft_strtrim(str, " ");
 	i = 0;
 	while (line[i])
 	{
-		len = 0;
-		while (line[i] && ft_isalpha(line[i]))
-		{
-			len++;
-			i++;
-		}
-		lstadd_token_node(ft_substr(line, i - len, len), token_list);
-
+		make_alpha_token(line, &i, token_list);
 		//yeblee - "" '' () 처리
 		//jibang - 나머지
 		while (line[i] && !ft_isalpha(line[i]))
