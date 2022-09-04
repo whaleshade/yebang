1d309088206dbf4974da4520917809dd7f936fc5
whaleshade <whaleshade@gmail.com>
2022-09-04 19:58:41 +0900

    unclosed quote error

--- srcs/syntax_error.c
+++ srcs/syntax_error.c
@@ -3,21 +3,25 @@
 static int	is_forbidden_char(char *c);
 static int	is_invalid_opt_syntax(t_list *list);
 static int	is_paired_parens(char *str);
+static int	is_unclosed_quote(char *str);
 
 //syntax error 리턴값별로 perror 문구 다르게
 int	syntax_error(t_list *list)
 {
 	while (list)
 	{
-		if (is_forbidden_char((char *)list->content))
+		if (is_forbidden_char((char *)list->content) == TRUE)
 			return (ERROR);
 		if (ft_strncmp(list->content, "&", 2) == 0)
 			return (ERROR);
 		if (ft_strncmp(list->content, ")", 2) == 0)
 			return (ERROR);
-		if (is_invalid_opt_syntax(list))
-			return (ERROR);
 		if (ft_strncmp(list->content, "(", 1) == 0 && is_paired_parens(list->content) == FALSE)
+			return (ERROR);
+		if (is_invalid_opt_syntax(list) == TRUE)
+			return (ERROR);
+		if ((ft_strncmp(list->content, "\"", 1) == 0 || ft_strncmp(list->content, "\'", 1) == 0)\
+			&& is_unclosed_quote(list->content) == TRUE)
 			return (ERROR);
 		list = list->next;
 	}
@@ -79,4 +83,26 @@ static int	is_paired_parens(char *str)
 	if (par_cnt != 0)
 		return (FALSE);
 	return (TRUE);
+}
+
+static int	is_unclosed_quote(char *str)
+{
+	int		i;
+	int		len;
+	int		result;
+
+	len = ft_strlen(str);
+	i = 0;
+	result = TRUE;
+	if (str[i] == '\'')
+	{
+		if (str[len - 1] == '\'')
+			result = FALSE;
+	}
+	else if (str[i] == '\"')
+	{
+		if (str[len - 1] == '\"')
+			result = FALSE;
+	}
+	return (result);
 }
