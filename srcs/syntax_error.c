27138316406e197ec70a33e6a9a60f65d3614817
whaleshade <whaleshade@gmail.com>
2022-09-04 19:11:15 +0900

    option syntax error

--- srcs/syntax_error.c
+++ srcs/syntax_error.c
@@ -1,17 +1,18 @@
 #include "../includes/minishell.h"
 
 static int	is_forbidden_char(char *c);
+static int	is_invalid_opt_syntax(t_list *node);
 
+//syntax error 리턴값별로 perror 문구 다르게
 int	syntax_error(t_list *list)
 {
-	t_list	*node;
-
-	node = list;
-	while (node)
+	while (list)
 	{
-		if (is_forbidden_char((char *)node->content))
+		if (is_forbidden_char((char *)list->content))
 			return (ERROR);
-		node = node->next;
+		if (is_invalid_opt_syntax(list))
+			return (ERROR);
+		list = list->next;
 	}
 	return (SUCCESS);
 }
@@ -21,16 +22,34 @@ static int	is_forbidden_char(char *c)
 	int		result;
 
 	result = FALSE;
-	// if (*c != '$' || *c != '&' || *c != '*' || *c != '('
-	// 	|| *c != ')' || *c != '=' || *c != '|' || *c != '\"'
-	// 	|| *c != '\'' || *c != '.' || *c != '?' || *c != '/'
-	// 	|| *c != '\\' || *c != '-' || *c != '<' || *c != '>'
-	// 	|| *c != ' ' || !ft_isalnum(*c) || *c != '\0' || *c != '\n')
-	// if (*c == '!' || *c == '@' || *c == '~' || *c == '#' || *c == '%' || *c == '^' || *c == '+' || *c == '{' || *c == '}' || *c == '[' || *c == ']' || *c == ':' || *c == ';' || *c == ',' || *c == '\\')
-
-	if (*c == '!')
+	if (*c == '!' || *c == '@' || *c == '~' || *c == '#'\
+		|| *c == '%' || *c == '^' || *c == '+' || *c == '{'\
+		|| *c == '}' || *c == '[' || *c == ']' || *c == ':'\
+		|| *c == ';' || *c == ',' || *c == '\\')
 	{
 		result = TRUE;
+	}
+	return (result);
+}
+
+static int	is_invalid_opt_syntax(t_list *list)
+{
+	int		result;
+
+	result = FALSE;
+	while (list)
+	{
+		if (list->next != NULL && ft_strncmp(list->next->content, "-", 1) == 0)
+		{
+			if (ft_strncmp(list->content, " ", 1) != 0)
+				result = TRUE;
+		}
+		if (ft_strncmp(list->content, "-", 1) == 0)
+		{
+			if (list->next != NULL && ft_strncmp(list->next->content, " ", 1) == 0)
+				result = TRUE;
+		}
+		list = list->next;
 	}
 	return (result);
 }
